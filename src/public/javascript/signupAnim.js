970abd8fc8375f6f55aa418a7aae198b88cf92f7
Joao Zsigmond <jzsiggy@gmail.com>
2019-11-10 00:55:08 -0200

    Adding styles and javascript for signup form

---
+++ src/public/javascript/signupAnim.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  const studentStartupRadio = document.querySelector(".studentStartupRadio");
+  const startupSignupDiv = document.querySelector(".startup-signup");
+  studentStartupRadio.addEventListener('change', () => {
+    console.log(studentStartupRadio.value);
+    if (studentStartupRadio.value === "student") {
+      startupSignupDiv.classList = "startup-signup hide";
+    } else {
+      startupSignupDiv.classList = "startup-signup";
+    }
+  })
+});
